4f6d66f83df6a698ff8dd5dba088cac63a8d57e9
Ejaz <ejazdhalayat45@gmail.com>
2024-02-03 20:36:32 +0530

    project file update

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -69,38 +69,46 @@ const projects = [
 export function Project() {
   return (
     <Container id="project">
-      <h2>My Projects</h2>
-      <div className="projects">
-        {projects.map((project, index) => (
-          <div key={index} >
-            <div className="project">
-              <header>
-                <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
-                  <title>Folder</title>
-                  <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
-                </svg>
-                <div className="project-links">
-                  <a href="#" target="_blank" rel="noreferrer">
-                    <img src={externalLink} alt="Visit site" />
-                  </a>
-                </div>
-              </header>
-              <div className="body">
-                <h3>{project.title}</h3>
-                <h4>{project.date}</h4>
-                <br></br>
-                <p>{project.description}</p>
-
-              </div>
-              <footer>
-                <ul className="tech-list">
-                  <li>{project.technologies.join(", ")}</li>
-                </ul>
-              </footer>
+    <h2>My Projects</h2>
+    <div className="projects">
+      {projects.map((project, index) => (
+        <div key={index} className="project">
+          <header>
+            <svg
+              width="50"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#23ce6b"
+              strokeWidth="1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <title>Folder</title>
+              <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+            </svg>
+            <div className="project-links">
+              {/* Replace '#' with the actual project URL */}
+              <a href="#" target="_blank" rel="noreferrer">
+                <img src={externalLink} alt="Visit site" />
+              </a>
             </div>
+          </header>
+          <div className="body">
+            <h3>{project.title}</h3>
+            <h4>{project.date}</h4>
+            <br />
+            <p>{project.description}</p>
           </div>
-        ))}
-      </div>
-    </Container>
+          <footer>
+            <ul className="tech-list">
+              <li>{project.technologies.join(", ")}</li>
+            </ul>
+          </footer>
+        </div>
+      ))}
+    </div>
+  </Container>
   );
 }
